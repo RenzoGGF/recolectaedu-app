7da1a1ecb2aef415beb7419661154706a48a42a0
Seroluce <141604179+Seroluce@users.noreply.github.com>
2025-11-15 20:22:36 -0500

    feat(course): añadir componente de recursos recientes con soporte para filtrado por tipo y estado de carga

---
+++ src/app/features/course/components/recent-resources/recent-resources.ts
@@ -0,0 +1,57 @@
+import {ChangeDetectionStrategy, Component, inject, input, OnInit, signal} from '@angular/core';
+
+import {Resource, ResourceType} from '../../../../core/models/resource.model';
+import {ResourceService} from '../../../../core/services/resource.service';
+import {catchError, of, tap} from 'rxjs';
+import {HttpErrorResponse} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-recent-resources',
+  imports: [CommonModule],
+  templateUrl: './recent-resources.html',
+  styleUrl: './recent-resources.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RecentResources implements OnInit {
+  courseId = input.required<number>();
+
+  resources = signal<Resource[]>([]);
+  loadingMessage = signal('Cargando recursos recientes...');
+  isLoading = signal<boolean>(false);
+
+  private resourceService = inject(ResourceService);
+
+  ngOnInit(): void {
+    this.loadRecentResources();
+  }
+
+  filterByType(tipo: ResourceType): Resource[] {
+    return this.resources().filter(r => r.tipo === tipo);
+  }
+
+  private loadRecentResources(): void {
+    this.isLoading.set(true);
+    this.loadingMessage.set('Cargando recursos recientes...');
+    this.resources.set([]);
+
+    this.resourceService
+      .getRecientesPorCurso(this.courseId())
+      .pipe(
+        tap((data: Resource[]) => {
+          this.resources.set(data);
+          if (data.length === 0) {
+            this.loadingMessage.set('Este curso aún no tiene recursos.');
+          }
+        }),
+        catchError((err: HttpErrorResponse) => {
+          console.error('Error al cargar recursos recientes:', err);
+          this.loadingMessage.set('Error al cargar los recursos.');
+          return of([]);
+        })
+      )
+      .subscribe(() => {
+        this.isLoading.set(false);
+      });
+  }
+}
